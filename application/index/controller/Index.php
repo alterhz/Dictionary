d3f70ca86b92eab2c37fc6ee0cc3060c97ed828b
hanzhe <550360139@qq.com>
2019-05-24 11:58:29 +0800

    模块绑定index

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,19 +9,6 @@ class Index extends \think\Controller
 {
 	
 	
-	/**************************************************
-	 * 以下需要session检测
-	 *************************************************/
-	
-	/**
-	 * session检测
-	 */
-	protected function checkSession() {
-		if (!session('?session_user')) {
-			$this->redirect("index/login");
-		}
-	}
-	
 	public function register() {
 		if (input('?name') && input('?password')) 
 		{
@@ -64,6 +51,7 @@ class Index extends \think\Controller
 	public function login() {
 		if (input('?name') && input('?password')) 
 		{
+			echo "ddd";
 			$name = input('name');
 			$password = input('password');
 			
@@ -83,7 +71,7 @@ class Index extends \think\Controller
 			if (strcasecmp($user->password, $password) == 0) {
 				// 密码正确
 				session("session_user", $user);
-
+				echo "ddd";
 				return $this->redirect("index/index");
 			} else {
 				return $this->error("password is invalid.");
@@ -101,6 +89,19 @@ class Index extends \think\Controller
 	public function logout() {
 		session("session_user", null);
 		return $this->redirect("index/login");
+	}
+	
+	/**************************************************
+	 * 以下需要session检测
+	 *************************************************/
+	
+	/**
+	 * session检测
+	 */
+	protected function checkSession() {
+		if (!session('?session_user')) {
+			$this->redirect("index/login");
+		}
 	}
 	
 	public function index() {
@@ -189,6 +190,8 @@ class Index extends \think\Controller
 	}
 	
 	public function changeDic() {
+		$this->checkSession();
+		
 		if (input('?name')) {
 			$name = input('name');
 			if (strlen($name) > 0) {
@@ -202,6 +205,8 @@ class Index extends \think\Controller
 	}
 	
 	public function changeHistoryCount() {
+		$this->checkSession();
+		
 		if (input('?count')) {
 			$count = input('count');
 			if ($count >= 5) {
@@ -215,6 +220,8 @@ class Index extends \think\Controller
 	} 
 
 	public function changeTopCount() {
+		$this->checkSession();
+		
 		if (input('?count')) {
 			$count = input('count');
 			if ($count >= 5) {
@@ -227,35 +234,6 @@ class Index extends \think\Controller
 		return $this->redirect('index/index');
 	} 
 	
-    public function test3()
-    {
-        return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="https://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="https://e.topthink.com/Public/static/client.js"></script><think id="ad_bd568ce7058a1091"></think>';
-    }
-	
-	public function hello($name)
-	{
-		return 'hello.'.$name;
-	}
-	
-	public function test()
-	{
-		$r = User::get(1);
-		
-		return 'hello.'.$r;
-	}
-	
-	public function tests() {
-		$r = User::getByName('admin');
-		
-		$list = Search::where('user_id', $r->id)->order('datetime desc')->select();
-		
-		$sss = 'search<br/>';
-		foreach ($list as $l) {
-			$sss .= $l->id . '-' . $l->user_id . '-' .$l->text . '<br/>';
-		}
-		
-		return $sss;
-	}
-	
+   
 	
 }
